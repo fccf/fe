ff520628d434c1b10d2ed1e9dc676b2107f54537
fc <xiyanfc@gmail.com>
2018-12-20 22:40:14 +0800

    fortran common environment parameter

---
+++ src/fc/fc_env.f90
@@ -0,0 +1,17 @@
+module fc_env
+  !< fortran common environment parameter
+  use iso_fortran_env, &
+  only: int32, int64, real32, real64, input_unit, output_unit, error_unit
+  implicit none
+
+  integer, parameter :: i4p = int32
+  integer, parameter :: i8p = int64
+  integer, parameter :: r4p = real32
+  integer, parameter :: r8p = real64
+  integer, parameter :: ip_ = i4p          !< default integer kind
+  integer, parameter :: rp_ = r8p          !< default real kind
+  integer, parameter :: iu_ = input_unit   !< default input unit
+  integer, parameter :: ou_ = output_unit  !< default output unit
+  integer, parameter :: eu_ = error_unit   !< default error unit
+
+end module fc_env
