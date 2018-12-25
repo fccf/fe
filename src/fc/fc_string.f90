61c0eaf69bb728c0336a2edfe75959da0010d456
fc <xiyanfc@gmail.com>
2018-12-25 23:44:37 +0800

    debug test

--- src/fc/fc_string.f90
+++ src/fc/fc_string.f90
@@ -8,10 +8,8 @@ module fc_string
 
   public :: is_logical, is_int, is_real
   public :: to_logical, to_int, to_real
-  public :: to_str
 
-  public :: file_dir, file_base, file_ext, file_name
-  public :: file_content, next_line_content
+  public :: to_str
 
   private
 
@@ -22,8 +20,6 @@ module fc_string
   interface to_str
     module procedure :: scalar_to_string, vector_to_string, matrix_to_string
   end interface to_str
-
-  character(*), parameter :: linux_path_separator_ = '/'
 
 contains
   !=============================================================================
@@ -437,127 +433,5 @@ contains
     if(lshell) s = s//']'
 
   end function matrix_to_string
-  !=============================================================================
-  !!> filename string operation
-  !=============================================================================
-  function file_dir(this, sep)
-    !< Return the directory name of a string containing a file name.
-    character(*), intent(in)  :: this     !< The string.
-    character(*), intent(in)  :: sep      !< Directory separator.
-    character(:), allocatable :: file_dir !< Directory name.
-    optional :: sep
-
-    character(:), allocatable :: sep_     !< Separator, default value.
-    integer                   :: pos      !< Character position.
-
-    sep_ = linux_path_separator_
-    if (present(sep)) sep_ = sep
-    pos = index(this, sep_, back=.true.)
-    if (pos>0) file_dir = this(1:pos-1)
-
-  endfunction file_dir
-  !=============================================================================
-  function file_name(this, sep)
-    !< Return file name
-    character(*), intent(in)  :: this      !< The string.
-    character(*), intent(in)  :: sep       !< Directory separator.
-    character(:), allocatable :: file_name !< file name.
-    optional :: sep
-
-    character(:), allocatable :: sep_     !< Separator, default value.
-    integer                   :: pos      !< Character position.
-
-    sep_ = linux_path_separator_
-    if (present(sep)) sep_ = sep
-    pos  = index(this, sep_, back = .true.)
-    file_name = this(pos+1:)
-
-  end function file_name
-  !=============================================================================
-  function file_base(this, sep)
-    !< Return file base name
-    character(*), intent(in)  :: this      !< The string.
-    character(*), intent(in)  :: sep       !< Directory separator.
-    character(:), allocatable :: file_base !< Base file name.
-    optional :: sep
-
-    character(:), allocatable :: sep_      !< Separator, default value.
-    integer :: st,ed
-
-    sep_ = linux_path_separator_
-    if (present(sep)) sep_ = sep
-    st  = index(this, sep_, back = .true.)
-    ed = index(this, '.', back = .true.)
-    file_base = this(st+1:ed-1)
-
-  end function file_base
-  !=============================================================================
-  function file_ext(this)
-    character(*), intent(in)  :: this     !< The string.
-    character(:), allocatable :: file_ext !< Base file name.
-
-    integer :: pos      !< Character position.
-
-    pos  = index(this, '.', back = .true.)
-    file_ext = this(pos+1:)
-
-  end function file_ext
-  !=============================================================================
-  function file_content(file) result(string)
-    !< Read a file as a single string stream.
-    character(len=*), intent(in) :: file       !< File name.
-    character(:),allocatable     :: string     !< The string.
-
-    integer   :: unit       !< Logical unit.
-    logical   :: exist      !< Check if file exist.
-    integer   :: size   !< Size of the file.
-
-    inquire(file=file, exist=exist)
-    if (exist) then
-      open(newunit=unit, file=file, access='stream', form='unformatted')
-      inquire(file=file, size=size)
-      allocate(character(len=size) :: string)
-      read(unit=unit) string
-      close(unit)
-    else
-      error stop "file not found"
-    endif
-
-  end function file_content
-  !=============================================================================
-  function next_line_content(unit, form, ios) result(line)
-    !< Read line from a connected unit.
-    integer,      intent(in)  :: unit  !< Logical unit.
-    character(*), intent(in)  :: form  !< Format of unit.
-    character(:), allocatable :: line  !< Line storage.
-    integer, intent(out)      :: ios
-    optional :: form, ios
-
-    character(:), allocatable :: form_
-    integer    :: ios_ !< IO status code, local variable.
-    character  :: ch   !< Character storage.
-
-    form_ = 'formatted' ; if (present(form)) form_ = lower(form)
-    line = ''
-    select case(form_)
-    case('formatted')
-       do
-          read(unit, "(a)", advance='no', iostat=ios_) ch
-          if(is_iostat_eor(ios_)) exit
-          line = line//ch
-       enddo
-    case('unformatted')
-       do
-          read(unit, iostat=ios_) ch
-          if (ch==new_line('a')) then
-             ios_ = iostat_eor
-             exit
-          endif
-          line = line//ch
-       enddo
-    endselect
-    if(present(ios)) ios = ios_
-
-  endfunction next_line_content
 
 end module fc_string
